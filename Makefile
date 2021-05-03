bccd8ec3655cd2d35ce405888aad332e8abffc9f
Alexandru-Ioan Plesoiu <alexandru@iteachings.org>
2021-05-03 17:23:18 +0300

    Writting/Reading from drive from Sectors

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ all: compile run
 
 compile:
 	nasm -f bin ./boot.asm -o ./boot.bin
-
+	dd if=./message.txt >> ./boot.bin
+	dd if=/dev/zero bs=512 count=1 >> ./boot.bin
 run:
 	qemu-system-x86_64 -hda ./boot.bin
--- boot.asm
+++ boot.asm
@@ -1,72 +1,73 @@
-ORG 0 										; Set the origin to 0
-BITS 16 									; Tell nasm to assemble for 16 bits
+ORG 0
+BITS 16
 
 ; BIOS PARAMETER BLOCK
 _start:
 	jmp short start
 	nop
-
-times 33 db 0 ; Create 33 bytes of 0 for BIOS PARAMETER BLOCK
+; Create 33 bytes for BPB
+times 33 db 0 
 
 start:
-	jmp 0x7c0:step2 					; Setting code segment to 0x7c0
+	; Set code segment to 0x7c0
+	jmp 0x7c0:step2
 
-
-custom_interrupt:
-	mov ah, 0eh
-	mov al, 'k'
-	mov bl, 02h
-	int 0x10
-	iret
-
-interrupt_one:
-	mov ah, 0eh
-	mov al, '1'
-	mov bl, 02h
-	int 0x10
-	iret
 
 
 step2:
-	cli 										; Clear Interrupts
+	cli ; Clear Interrupts
 	; SETTING SEGMENTS TO 0x7c0
-	mov ax, 0x7c0 					
+	mov ax, 0x7c0 
 	mov ds, ax
 	mov es, ax
 	mov ax, 0x00
 	mov ss, ax
 	mov sp, 0x7c00
-	sti 										; Enables Interrupts
+	sti ;	Enables Interrupts
 	
-	; SETTING THE INTERRUPT VECTOR TABLE
-	mov word[ss:0x00], custom_interrupt
-	mov word[ss:0x02], 0x7c0
-	mov word[ss:0x04], interrupt_one
-	mov word[ss:0x06], 0x7c0
+	; READ FROM DISK
+	mov ah, 0x02 ; READ SECTOR COMMAND
+	mov al, 0x01 ; ONE SECTOR TO READ
+	mov ch, 0x00 ; Cylinder low eight bits
+	mov cl, 0x02 ; Read sector two
+	mov dh, 0x00 ; Head Number
+	mov bx, buffer 
+	int 0x13
+	jc error ; jump if carry flag is set
 
-	int 0x01 ; CALL OUR CUSTOM INTERRUPT
+	mov si, buffer
+	call print
 
-	mov si, message 				; Load into si register, 'message' start address	
-	call print 							; Call Print Function
-	jmp $ 									; Jump to itself (infinite loop)
+	jmp $
+
+error:
+	mov si, error_message
+	call print
+	jmp $
+
 
 print:
-	mov bl, 02h 						; Foreground color, ONLY IN GRAPHIC MODE (use bl register)
-.loop:
-	lodsb 									; load into al register from si register and increment pointer
-	call print_char 				; Print char
-	cmp al, 0 							; compare al and 0
-	je .done 								; if al == 0 jump to .done
-	jmp .loop 							; else jump to .loop
-.done:
+	mov bl, 02h	; Foreground color
+
+	.loop:
+	lodsb	; load into al register from si register and increment pointer
+	call print_char
+	cmp al, 0
+	je .done
+	jmp .loop
+	
+	.done:
 	ret
 
 print_char:
-	mov ah, 0eh 						; 14 => teletype output
-	int 0x10 								; Call BIOS Function
+	mov ah, 0eh	; 14 => teletype output
+	int 0x10
 	ret
 
-message: db 'MGE OS BOOTLOADER !!', 0
+error_message: db 'Failed to load sector', 0
 
 times 510 - ($ - $$) db 0 ; Minimum 510 Bytes
-dw 0xAA55 								; Set Bootloader Signature
+dw 0xAA55	; Set Bootloader Signature
+
+buffer:
+
