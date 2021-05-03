4053c9fc8a58afa6c820aeaa699975698673a213
Alexandru-Ioan Plesoiu <alexandru@iteachings.org>
2021-05-03 16:06:22 +0300

    Setting interrupt table/BIOS parameter block

--- boot.asm
+++ boot.asm
@@ -1,7 +1,41 @@
-ORG 0x7C00 								; Set the origin to 0x7C00
+ORG 0 										; Set the origin to 0
 BITS 16 									; Tell nasm to assemble for 16 bits
 
+; BIOS PARAMETER BLOCK
+_start:
+	jmp short start
+	nop
+
+times 33 db 0 ; Create 33 bytes of 0 for BIOS PARAMETER BLOCK
+
 start:
+	jmp 0x7c0:step2 					; Setting code segment to 0x7c0
+
+
+custom_interrupt:
+	mov ah, 0eh
+	mov al, 'k'
+	mov bl, 02h
+	int 0x10
+	iret
+
+step2:
+	cli 										; Clear Interrupts
+	; SETTING SEGMENTS TO 0x7c0
+	mov ax, 0x7c0 					
+	mov ds, ax
+	mov es, ax
+	mov ax, 0x00
+	mov ss, ax
+	mov sp, 0x7c00
+	sti 										; Enables Interrupts
+	
+	; SETTING THE INTERRUPT VECTOR TABLE
+	mov word[ss:0x00], custom_interrupt
+	mov word[ss:0x02], 0x7c0
+	
+	int 0x00 ; CALL OUR CUSTOM INTERRUPT
+
 	mov si, message 				; Load into si register, 'message' start address	
 	call print 							; Call Print Function
 	jmp $ 									; Jump to itself (infinite loop)
@@ -22,7 +56,7 @@ print_char:
 	int 0x10 								; Call BIOS Function
 	ret
 
-message: db 'Hello World!!', 0
+message: db 'MGE OS BOOTLOADER !!', 0
 
 times 510 - ($ - $$) db 0 ; Minimum 510 Bytes
 dw 0xAA55 								; Set Bootloader Signature
